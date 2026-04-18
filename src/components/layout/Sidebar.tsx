e0346a7ba693938bb381841c2d652d8c78d1548d
Rain <rain@RaindeMacBook-Pro.local>
2026-04-18 20:41:36 +0800

    upload category func

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -33,25 +33,24 @@ export function Sidebar({ className, categories = [] }: { className?: string, ca
                 推荐资源
               </div>
             </Link>
-            
-              categories.length === 0 ? (
-                <div className="px-4 py-2 text-sm text-muted-foreground">暂无分类数据</div>
-              ) : (
+
+            categories.length === 0 ? (
+            <div className="px-4 py-2 text-sm text-muted-foreground">暂无分类数据</div>
+            ) : (
                 categories.map((category) => (
-                <Link key={category.id} href={`/category/${category.slug}`}>
-                  <div
-                    className={cn(
-                      "w-full flex items-center justify-start rounded-md px-4 py-2 text-sm font-medium hover:bg-accent hover:text-accent-foreground transition-colors",
-                      pathname === `/category/${category.slug}`
-                        ? "bg-accent text-accent-foreground font-bold text-primary"
-                        : "transparent text-muted-foreground"
-                    )}
-                  >
-                    {category.name}
-                  </div>
-                </Link>
-              ))
-            )}
+            <Link key={category.id} href={`/category/${category.slug}`}>
+              <div
+                className={cn(
+                  "w-full flex items-center justify-start rounded-md px-4 py-2 text-sm font-medium hover:bg-accent hover:text-accent-foreground transition-colors",
+                  pathname === `/category/${category.slug}`
+                    ? "bg-accent text-accent-foreground font-bold text-primary"
+                    : "transparent text-muted-foreground"
+                )}
+              >
+                {category.name}
+              </div>
+            </Link>
+            ))
           </div>
         </div>
       </div>
